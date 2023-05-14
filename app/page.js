3c4831042c445e5eefeca7abb49c5719311b4a8c
DanielaDiazG <daniela.diazgomez@mercadolibre.com.co>
2023-05-14 17:52:24 -0500

    feat: base structure

---
+++ app/page.js
@@ -0,0 +1,11 @@
+"use client";
+import { Button, Grid, Stack } from "@mui/material";
+import SignIn from "@/components/SignIn";
+
+export default function Home() {
+  return (
+      <main>
+          <SignIn/>
+      </main>
+  );
+}
